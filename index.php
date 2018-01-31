e864aba8ca5c24b11e1abaead9e2379b4f952ba8
Bruno <brunosouza2011e@github.com>
2018-01-31 17:49:49 -0200

    Imagem com proporção aceitável
    #4 fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
 <p>Hello World! </p>
 <p>Aqui farei meus testes com div etc... </p>
-<img src="ss.jpg"  width="300" height="100">
+<img src="ss.jpg"  width="100" height="100">
 </body> 
 </html>
 <?php
