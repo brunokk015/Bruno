cc32b3ba24d3e8d84a9a0d8a1e626e2474b129a6
Bruno <brunosouza2011e@github.com>
2018-01-31 17:46:30 -0200

    Estudando adição de imagem
    Familiarizando com o github

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<html>
+<Head>
+<title>abcd</title>
+</Head>
+<body>
+<p>Hello World! </p>
+<p>Aqui farei meus testes com div etc... </p>
+<img src="ss.jpg"  width="300" height="100">
+</body> 
+</html>
 <?php
-
-echo "My world!";
-
+echo "Primeiro teste"
 ?>
+
