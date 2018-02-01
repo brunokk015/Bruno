0e25695b97d24351c75d4339b4e1f347e6dd5775
Bruno <brunosouza2011e@github.com>
2018-02-01 13:02:01 -0200

    FIX

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-     <title>Recomeço</title>
-     <meta charset="utf-8">
-     <style>
-         font-family: Arial,sans-serif;
-     </style>
-     <script>
-Function initial (){
-    document.getElementById("nome").innerHTML= "Bruno";
-}
-window.onload= initial;
-     </script>
-</head>
-<body>
-    <!--comentário teste-->
-    <p id="nome"></p>
-    Retrabalhando &nbsp; tudo desde o inicio ççç´´´~~~
-    15,99 &euro; 
-</body>
+<html lang="pt-BR" >
+    <head>
+        <title>Unidade 02 - Elemento Head</title>
+        <meta charset="utf-8">
+        <style>
+            body {
+                background: #f00;
+                font-family: Arial,sans-serif;
+                color:oldlace
+                
+            }
+        </style>
+        <script>
+            function inicial() {
+                nome.innerHTML= "Agora foi"
+            }
+        
+        window.onload= inicial;
+        </script>
+    </head>
+    <body>
+        <p id="nome"></p>
+        Trocando a fonte
+    </body>
 </html>
